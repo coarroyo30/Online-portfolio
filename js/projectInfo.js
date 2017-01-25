bf67dc03730d323db2078377cf126893cee4ad46
coarroyo30 <coarroyo30@gmail.com>
2017-01-25 13:24:09 -0500

    modified url to projectInfo.js

--- js/projectInfo.js
+++ js/projectInfo.js
@@ -1,40 +1,40 @@
 var projects = [
 {
 	"name" : "Responsive layout with Sass",
-	"url" : "http://coarroyo30.github.io/CSS-Sass/",
-	"github" : "http://github.com/coarroyo30/CSS-Sass",
+	"url" : "http://coarroyo30.github.io/responsive/",
+	"github" : "http://github.com/coarroyo30/responsive",
 	"description" : "This project was originally done using CSS. It was then remade using Sass to make the code more maintainable. This project also displays the different breakpoints for mobile, tablets, and desktop devices.",
 	"preview" : "project-5.jpg",
 	"tech" : ["html", "css", "sass", "github"]
 },
 {
 	"name" : "Interactive video player",
-	"url" : "http://coarroyo30.github.io/interactive-video-player/",
-	"github" : "http://github.com/coarroyo30/interactive-video-player",
+	"url" : "http://coarroyo30.github.io/video/",
+	"github" : "http://github.com/coarroyo30/video",
 	"description" : "This project was made with html, css, Sass, and Javascript. The controls for the video were custom made. As the video plays the text beneath the video is being highlighted.",
 	"preview" : "project-7.jpg",
 	"tech" : ["html", "css", "sass", "js", "github"]
 },
 {
 	"name" : "API gallery",
-	"url" : "http://coarroyo30.github.io/API-gallery/",
-	"github" : "http://github.com/coarroyo30/API-gallery",
+	"url" : "http://coarroyo30.github.io/api/",
+	"github" : "http://github.com/coarroyo30/api",
 	"description" : "This project was made with the Spotify API. Ajax is used to get information from the API and display it in a gallery. If an image is click a lightbox will appear, displaying all the information of the items that has been clicked.",
 	"preview" : "project-10.jpg",
 	"tech" : ["html", "css", "sass", "js", "jquery", "github"]
 },
 {
 	"name" : "Web app dashboard",
-	"url" : "http://coarroyo30.github.io/Web-app-dashboard/",
-	"github" : "http://github.com/coarroyo30/Web-app-dashboard",
+	"url" : "http://coarroyo30.github.io/app/",
+	"github" : "http://github.com/coarroyo30/app",
 	"description" : "This project is a web app dashboard. It was made Javascript driven charts and graphs. It is a front end app only. ",
 	"preview" : "project-9.jpg",
 	"tech" : ["html", "css", "sass", "js", "github"]
 },
 {
 	"name" : "Performance optimization",
-	"url" : "http://coarroyo30.github.io/performance-optimization/",
-	"github" : "http://github.com/coarroyo30/performance-optimization",
+	"url" : "http://coarroyo30.github.io/performance/",
+	"github" : "http://github.com/coarroyo30/performance",
 	"description" : "In this project the performance of the website had to be optimized. The original project had 43 network requests, and it was remade to have 26 network requests. Images were resized, and the overall website was made to load faster.",
 	"preview" : "project-11.jpg",
 	"tech" : ["html", "css", "sass", "js", "github"]
